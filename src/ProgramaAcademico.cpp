2b94a099ff3fb062f69591117286d8357bce2f2b
Daniel Posada <daniel.lejandr0o0@gmail.com>
2024-10-16 22:47:52 -0500

    Se implementaron las últimas funciones auxiliares que permitan mostrar la diferencia porcentual anual en cantidad de nuevos estudiantes para diferentes programas académicos. Entre otras funcionalidades.

--- src/ProgramaAcademico.cpp
+++ src/ProgramaAcademico.cpp
@@ -1,8 +1,13 @@
 
 #include "ProgramaAcademico.h"
 #include "Consolidado.h"
+#include <iostream>
+
 
 using std::string;
+using std::cout;
+using std::endl;
+
 
 ProgramaAcademico::~ProgramaAcademico() {
     for (auto const &[key, value] : consolidados) {
@@ -10,13 +15,16 @@ ProgramaAcademico::~ProgramaAcademico() {
     }
 }
 
+
 void ProgramaAcademico::setDato(string const &atributo, string valor) {
     datos[atributo] = std::move(valor);
 }
 
+
 string ProgramaAcademico::getDato(string const &atributo) {
     return datos[atributo];
 }
+
 
 bool ProgramaAcademico::sinMatriculasNuevas() const {
     bool sinMatriculas = false;
@@ -36,19 +44,72 @@ bool ProgramaAcademico::sinMatriculasNuevas() const {
     return sinMatriculas;
 }
 
+
 void ProgramaAcademico::addConsolidado(int semestre, Consolidado *consolidado) {
     consolidados[semestre] = consolidado;
 }
 
-/*
-void ProgramaAcademico::mostrarMatriculadosConsolidado(int anio) {
-    consolidados[semestre]->mostrarMatriculados();
+
+int ProgramaAcademico::getTotalMatriculadosPorAnio(int anio) {
+    int semestre1 = anio * 10 + 1;
+    int semestre2 = semestre1 + 1;
+    int cantidadMatriculados = 0;
+
+    if (auto it1 = consolidados.find(semestre1) && it1 != consolidados.end()) {
+        cantidadMatriculados += it1->second->getMatriculados();
+    }
+
+    if (auto it2 = consolidados.find(semestre2) && it2 != consolidados.end()) {
+        cantidadMatriculados += it2->second->getMatriculados();
+    }
+    return cantidadMatriculados;
 }
 
-void ProgramaAcademico::mostrarDataConsolidado(int semestre) {
-    consolidados[semestre]->mostrarData();
+
+int ProgramaAcademico::getNuevosMatriculadosPorAnio(int) {
+
 }
-*/
+
+
+void ProgramaAcademico::mostrarMatriculadosConsolidado(const pair<int, int> &aniosAnalisis) {
+    const int limiteInferior = aniosAnalisis.first;
+    const int limiteSuperior = aniosAnalisis.second;
+    string const LLAVENOMBREPROGRAMA = "programaacademico";
+    cout << "El número de matriculados registrados para el programa " << this->getDato(LLAVENOMBREPROGRAMA) << " entre los años ";
+    cout << limiteInferior << " y " << limiteSuperior << "es :" << endl;
+    for(int i = limiteInferior; i <= limiteSuperior; i++) {
+        cout << i << ": " << getTotalMatriculadosPorAnio(i) << endl;
+    }
+}
+
+
+void ProgramaAcademico::mostrarDiferenciaPorcentualNuevosMatriculados(const pair<int, int> &aniosAnalisis) {
+    const int limiteInferior = aniosAnalisis.first;
+    const int limiteSuperior = aniosAnalisis.second;
+    const int n = limiteSuperior - limiteInferior + 1;
+
+    int cantMatriculadosAnterior = 0;
+    int cantMatriculadosActual = getNuevosMatriculadosPorAnio(limiteInferior);
+    int porcentaje = 0;
+
+    for (int i = 1; i < n; i++) {
+        cantMatriculadosAnterior = cantMatriculadosActual;
+        cantMatriculadosActual = getTotalMatriculadosPorAnio(limiteInferior + i);
+        porcentaje = (cantMatriculadosActual * 100) / cantMatriculadosAnterior;
+
+        cout << "La diferencia porcentual de nuevos matriculados entre los años "
+             << limiteInferior + i - 1 << " y " << limiteInferior + i << " es: "
+             << porcentaje << "%" << endl;
+    }
+}
+
+
+void ProgramaAcademico::mostrarIdentificadoresPrograma() {
+    cout << "Código SNIES: " << this->getDato("codigosnies") << endl;
+    cout << "Nombre del programa académico: " << this->getDato("programaacademico") << endl;
+}
+
+
 
 
 
--- src/ProgramaAcademico.h
+++ src/ProgramaAcademico.h
@@ -7,11 +7,13 @@
 
 using std::string;
 using std::map;
+using std::pair;
 
 class ProgramaAcademico {
     map<string, string> datos;
     map<int, Consolidado *> consolidados;
-
+    int getTotalMatriculadosPorAnio(int);
+    int getNuevosMatriculadosPorAnio(int);
 public:
     ProgramaAcademico() = default;
     ~ProgramaAcademico();
@@ -19,8 +21,19 @@ public:
     string getDato(string const &);
     bool sinMatriculasNuevas() const;
     void addConsolidado(int, Consolidado *);
-    void mostrarMatriculadosConsolidado(int);
-    void mostrarDataConsolidado(int);
+
+    // Estas funciones se encargan de mostrar los datos de los consolidados en los años correspondientes
+    // donde el primer entero de la pareja es el límite inferior y el segundo el límite superior, incluyendolos.
+    void mostrarMatriculadosConsolidado(const pair<int, int> &);
+    void mostrarDiferenciaPorcentualNuevosMatriculados(const pair<int, int> &);
+    // Esta función muestra el código SNIES y el nombre del programa académico.
+    void mostrarIdentificadoresPrograma();
 };
 
 #endif
+
+/*
+    1. El mapa datos es un mapá que contiene los datos del programa académico, teniendo como llaves
+    los nombres de los datos o columnas donde se ubican en minúsculas y sin espacios.
+
+*/
