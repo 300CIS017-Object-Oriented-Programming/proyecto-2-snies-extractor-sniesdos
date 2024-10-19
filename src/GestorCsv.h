1568393406f656119c48d8cdb26237646363cbcc
migueangel1228 <miguelangel1228@javerianacali.edu.co>
2024-10-19 09:01:20 -0500

    29-solución-de-error-en-la-clase-gestorcsv - FIX: Solución de problema std

--- src/GestorCsv.h
+++ src/GestorCsv.h
@@ -1,14 +1,19 @@
+// After
 #ifndef GESTOR_CSV_H
 #define GESTOR_CSV_H
 
 #include <string>
 #include <vector>
 #include <map>
+#include <list>
 #include <fstream>
+#include "ProgramaAcademico.h"
 
 using std::string;
 using std::vector;
 using std::map;
+using std::list;
+
 class GestorCsv {
 public:
     GestorCsv() = default;
@@ -17,6 +22,7 @@ public:
     vector<vector<string>> leerArchivoPrimera(string &rutaBase, string &ano, vector<int> &codigosSnies);
     vector<vector<string>> leerArchivoSegunda(string &rutaBase, string &ano, vector<int> &codigosSnies);
     vector<vector<string>> leerArchivo(string &rutaBase, string &ano, vector<int> &codigosSnies, int colmunaCodigoSnies);
+
     bool crearArchivo(string &ruta, map <int, ProgramaAcademico *> &mapadeProgramasAcademicos, vector<string> etiquetasColumnas);
     bool crearArchivoBuscados(string &ruta, list<ProgramaAcademico *> &programasBuscados, vector<string> etiquetasColumnas);
     bool crearArchivoExtra(string &ruta, vector<vector<string>> datosAImprimir);
